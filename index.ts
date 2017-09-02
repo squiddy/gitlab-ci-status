b0a3f21a7ca4c32a74069dac2c2311d0eba3b0a1
Reiner Gerecke <me@reinergerecke.de>
2017-09-02 12:39:43 +0200

    Remove default of app state file

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import { startServer } from "./src/server";
 
 startServer({
   port: Number(process.env.APP_PORT) || 3000,
-  persistPath: process.env.APP_STATE_FILE_PATH || "./appstate.json"
+  persistPath: process.env.APP_STATE_FILE_PATH
 });

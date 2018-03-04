936d5c4bcbf31a8220efabbe2e3ffe1c41aa410f
Reiner Gerecke <me@reinergerecke.de>
2018-03-04 17:53:06 +0100

    prepare proxy to api

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('/state').then(res => {
+      console.log(res);
+    })
+  }
+
   render() {
     return (
       <div className="App">

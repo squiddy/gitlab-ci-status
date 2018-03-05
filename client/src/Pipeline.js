4cc62ec3ca08a28e5ae12467d50124a3056dd972
Reiner Gerecke <me@reinergerecke.de>
2018-03-05 23:24:50 +0100

    Show total pipeline runtime

--- client/src/Pipeline.js
+++ client/src/Pipeline.js
@@ -3,7 +3,29 @@ import React from "react";
 import { Avatar } from "./Avatar";
 import { StatusIcon } from "./StatusIcon";
 
+export function getTotalBuildRunTimeMs(builds) {
+  return builds.reduce((sum, b) => {
+    if (!b.started_at) {
+      return sum;
+    }
+
+    const start = new Date(b.started_at.slice(0, -4));
+
+    if (!b.finished_at) {
+      return sum + (new Date() - start) / 1000;
+    }
+
+    const finish = new Date(b.finished_at.slice(0, -4));
+    return sum + (finish - start) / 1000;
+  }, 0);
+}
+
 export function Pipeline({ pipeline }) {
+  const duration = getTotalBuildRunTimeMs(pipeline.builds);
+  const isFinished = !["created", "pending", "running"].includes(
+    pipeline.status
+  );
+
   return (
     <div className={`queue-entry`}>
       <div className="queue-entry-body">
@@ -21,6 +43,11 @@ export function Pipeline({ pipeline }) {
         </div>
       </div>
       <div className="queue-entry-footer">
+        <div className="pipeline-duration">
+          {isFinished ? "took" : "running for"}
+          {' '}
+          {Math.floor(duration / 60)}m {duration % 60}s
+        </div>
         <PipelineGraph builds={pipeline.builds} />
       </div>
     </div>

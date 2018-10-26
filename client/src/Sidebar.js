d736f2c3dfecd99efaf71b876fd390e4ffda76a6
Reiner Gerecke <me@reinergerecke.de>
2018-10-26 21:46:59 +0200

    Make sidebar sticky

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -3,10 +3,10 @@ import React from "react"; // eslint-disable-line no-unused-vars
 export function Sidebar(props) {
   if (!props.isVisible) {
     return (
-      <aside className="px-4 my-4">
+      <aside className="px-4 max-h-screen sticky pin-t">
         <svg
           onClick={props.onToggleVisibility}
-          className="fill-current w-4 h-4 cursor-pointer text-grey"
+          className="fill-current my-4 w-4 h-4 cursor-pointer text-grey"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
         >
@@ -17,16 +17,18 @@ export function Sidebar(props) {
   }
 
   return (
-    <aside className="px-4 pr-8 py-4 items-strech bg-white">
-      <svg
-        onClick={props.onToggleVisibility}
-        className="fill-current w-4 h-4 cursor-pointer text-black block mb-8"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
-      </svg>
-      {props.children}
+    <aside className="px-4 pr-8 py-4 items-strech bg-white shadow max-h-screen sticky pin-t">
+      <div className="top-4">
+        <svg
+          onClick={props.onToggleVisibility}
+          className="fill-current w-4 h-4 cursor-pointer text-black block mb-8"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+        </svg>
+        {props.children}
+      </div>
     </aside>
   );
 }

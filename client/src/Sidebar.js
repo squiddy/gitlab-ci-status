1caed4abe13bb26187f9084c27ac18d5a40fc351
Reiner Gerecke <me@reinergerecke.de>
2018-10-26 22:31:33 +0200

    Add logo and project title to sidebar

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -1,5 +1,7 @@
 import React from "react"; // eslint-disable-line no-unused-vars
 
+import GitlabLogo from "./images/gitlab_logo.svg";
+
 export function Sidebar(props) {
   if (!props.isVisible) {
     return (
@@ -21,12 +23,19 @@ export function Sidebar(props) {
       <div className="top-4">
         <svg
           onClick={props.onToggleVisibility}
-          className="fill-current w-4 h-4 cursor-pointer text-black block mb-8"
+          className="fill-current w-4 h-4 cursor-pointer text-black block"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
         >
           <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
         </svg>
+
+        <div className="text-center mb-8">
+          <img className="w-24 mb-2" src={GitlabLogo} />
+          <h1 className="text-lg text-grey-dark font-light">
+            GitLab CI Status
+          </h1>
+        </div>
         {props.children}
       </div>
     </aside>

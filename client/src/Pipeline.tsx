a49f32f29ec8d166380c55dc46b484263709f8ed
Reiner Gerecke <reiner.gerecke@momox.biz>
2019-01-23 19:33:00 +0100

    Use react hooks for Pipeline

--- client/src/Pipeline.tsx
+++ client/src/Pipeline.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Avatar } from "./Avatar";
 import { StatusIcon } from "./StatusIcon";
@@ -24,87 +24,64 @@ export function getTotalBuildRunTimeMs(builds: BuildData[]): number {
   }, 0);
 }
 
-interface PipelineState {
-  showDetails: boolean;
-}
+export function Pipeline({ pipeline }: { pipeline: PipelineData }) {
+  const [showDetails, setShowDetails] = useState(
+    !isPipelineFinished(pipeline.status) || pipeline.status === "failed"
+  );
 
-export class Pipeline extends React.Component<
-  { pipeline: PipelineData },
-  PipelineState
-> {
-  constructor(props: any) {
-    super(props);
+  const duration = getTotalBuildRunTimeMs(pipeline.builds);
 
-    this.state = {
-      showDetails:
-        !isPipelineFinished(props.pipeline.status) ||
-        props.pipeline.status === "failed"
-    };
-    this.toggleDetails = this.toggleDetails.bind(this);
-  }
+  const navigateToGitLab = () => {
+    const url = `http://gitlab.bof.mm.local/${
+      pipeline._raw.project.path_with_namespace
+    }/pipelines/${pipeline.id}`;
+    window.open(url, "_blank");
+    window.focus();
+  };
 
-  toggleDetails() {
-    this.setState(state => ({ showDetails: !state.showDetails }));
-  }
+  const isMainRepository =
+    pipeline._raw.project.namespace !== pipeline._raw.user.username;
 
-  render() {
-    const { pipeline } = this.props;
-    const duration = getTotalBuildRunTimeMs(pipeline.builds);
+  const isFinished = isPipelineFinished(pipeline.status);
 
-    const navigateToGitLab = () => {
-      const url = `http://gitlab.bof.mm.local/${
-        pipeline._raw.project.path_with_namespace
-      }/pipelines/${pipeline.id}`;
-      window.open(url, "_blank");
-      window.focus();
-    };
-
-    const isMainRepository =
-      pipeline._raw.project.namespace !== pipeline._raw.user.username;
-
-    const isFinished = isPipelineFinished(pipeline.status);
-
-    return (
+  return (
+    <div
+      className={`flex flex-col mb-8 rounded cursor-pointer ${
+        isMainRepository ? "main-repository" : ""
+      }`}
+    >
       <div
-        className={`flex flex-col mb-8 rounded cursor-pointer ${
-          isMainRepository ? "main-repository" : ""
-        }`}
+        className="flex flex-row justify-between items-center bg-white rounded-t"
+        onClick={navigateToGitLab}
       >
-        <div
-          className="flex flex-row justify-between items-center bg-white rounded-t"
-          onClick={navigateToGitLab}
-        >
-          <div className="p-4 flex-initial w-48">
-            <Avatar className="h-16 rounded-full m-1" obj={pipeline.project} />
-            <Avatar className="h-16 rounded-full m-1" obj={pipeline.user} />
-          </div>
-          <div className="flex-1 px-6 py-4">
-            <div className="font-bold text-xl mb-2">
-              {pipeline.project.name}
-            </div>
-            <p className="text-grey-darker text-base">{pipeline.ref}</p>
-          </div>
-          <div className="p-4 flex-initial flex items-center">
-            <StatusIcon className="h-12 w-12" status={pipeline.status} />
-          </div>
+        <div className="p-4 flex-initial w-48">
+          <Avatar className="h-16 rounded-full m-1" obj={pipeline.project} />
+          <Avatar className="h-16 rounded-full m-1" obj={pipeline.user} />
         </div>
-        <div
-          className={"flex flex-col bg-indigo-darker rounded-b text-grey"}
-          onClick={this.toggleDetails}
-        >
-          {this.state.showDetails && (
-            <div className="border-b border-dotted border-indigo-darkest px-4 py-4">
-              <PipelineGraph pipeline={pipeline} />
-            </div>
-          )}
-          <div className="flex justify-between items-center w-full px-4 py-2">
-            <div className="text-xs">
-              {isFinished ? "took" : "running for"}{" "}
-              <Duration value={duration} ticking={!isFinished} />
-            </div>
+        <div className="flex-1 px-6 py-4">
+          <div className="font-bold text-xl mb-2">{pipeline.project.name}</div>
+          <p className="text-grey-darker text-base">{pipeline.ref}</p>
+        </div>
+        <div className="p-4 flex-initial flex items-center">
+          <StatusIcon className="h-12 w-12" status={pipeline.status} />
+        </div>
+      </div>
+      <div
+        className={"flex flex-col bg-indigo-darker rounded-b text-grey"}
+        onClick={() => setShowDetails(!showDetails)}
+      >
+        {showDetails && (
+          <div className="border-b border-dotted border-indigo-darkest px-4 py-4">
+            <PipelineGraph pipeline={pipeline} />
+          </div>
+        )}
+        <div className="flex justify-between items-center w-full px-4 py-2">
+          <div className="text-xs">
+            {isFinished ? "took" : "running for"}{" "}
+            <Duration value={duration} ticking={!isFinished} />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
